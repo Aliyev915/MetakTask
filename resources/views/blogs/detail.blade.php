07e78220f4704d86f50996729f94ab9aa009e74d
jedaiteam <fakhri.ahmadov@jedai.az>
2022-01-31 13:55:04 +0400

    Metak Task

--- resources/views/blogs/detail.blade.php
+++ resources/views/blogs/detail.blade.php
@@ -4,13 +4,13 @@
     <div class="container mt-5">
         <div class="col-md-8 offset-md-2">
             <div class="card">
-                <img src="{{ asset('images/img-01.jpg') }}" class="card-img-top" alt="...">
+                <img src="{{ asset('uploads/' . $blog->image) }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">{{ $blog->title }}</h5>
-                  <p>{{ $blog->created_at->format('Y M d h:i') }}</p>
-                  <p class="card-text">{{ $blog->description }}</p>
+                    <h5 class="card-title">{{ $blog->title }}</h5>
+                    <p>{{ $blog->created_at->format('Y M d h:i') }}</p>
+                    <p class="card-text">{{ $blog->description }}</p>
                 </div>
-              </div>
+            </div>
         </div>
     </div>
 @endsection
